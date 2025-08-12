837df270c90421a2762e8c7b4a6ffa414a10a918
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2025-08-12 00:29:54 -0600

    Append delay time in search location

--- app/presentation/routes/location.py
+++ app/presentation/routes/location.py
@@ -5,7 +5,8 @@ from typing import List
 from fastapi import APIRouter
 
 from app.core.exception import NotFoundException
-from app.shared.dependencies import NominatimDepend
+from app.shared.const import DEFAULT_MAX_DELAY_TIME_SEARCH_LOCATION
+from app.shared.dependencies import NominatimDepend, AuthUserCodeCredentials
 from app.shared.scheme.location import GeoLocationModel
 from app.shared.utils import async_task
 
@@ -13,8 +14,8 @@ location_route = APIRouter(prefix="/location", tags=["Location address"])
 
 
 @location_route.get("/search/{address}", response_model=List[GeoLocationModel])
-async def search_location(address: str, geolocator: NominatimDepend):
-    await asyncio.sleep(random.randint(1, 5))
+async def search_location(address: str, geolocator: NominatimDepend, code: AuthUserCodeCredentials):
+    await asyncio.sleep(random.randint(*DEFAULT_MAX_DELAY_TIME_SEARCH_LOCATION))
 
     results = await async_task(
         lambda s: geolocator.geocode(s, exactly_one=False),
