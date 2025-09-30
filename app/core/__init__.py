1dc28836c1ecde2b5c0803f5b17bc87626e0d46b
SealtielFreak <sealtielfreak@yandex.com>
2025-09-30 14:21:17 -0600

    Minor changes

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -22,6 +22,7 @@ def get_root_app() -> FastAPI:
         get_document_db()
 
         redis = get_async_cache()
+
         await redis.ping()
 
         yield
--- app/shared/dependencies/depends/db/__init__.py
+++ app/shared/dependencies/depends/db/__init__.py
@@ -9,29 +9,33 @@ from app.core import get_settings
 
 
 @functools.lru_cache
-def get_async_cache() -> redis.asyncio.Redis:
+def get_async_cache():
     settings = get_settings()
 
-    return redis.asyncio.Redis(
+    redis_client = redis.asyncio.Redis(
         host=settings.cache_host,
         port=settings.cache_port,
         decode_responses=True,
         username="default",
         password=settings.cache_password,
     )
+
+    return redis_client
 
 
 @functools.lru_cache
-def get_cache() -> redis.Redis:
+def get_cache():
     settings = get_settings()
 
-    return redis.Redis(
+    redis_client = redis.Redis(
         host=settings.cache_host,
         port=settings.cache_port,
         decode_responses=True,
         username="default",
         password=settings.cache_password,
     )
+
+    return redis_client
 
 
 @functools.lru_cache
