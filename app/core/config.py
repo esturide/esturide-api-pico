09819b803e5b776cee9888db40646eb7e480e490
SealtielFreak <sealtielfreak@yandex.com>
2026-02-01 17:09:02 -0600

    Fix environment variables

--- app/core/config.py
+++ app/core/config.py
@@ -13,7 +13,7 @@ class DefaultSettings(BaseSettings):
 
     mongodb_uri: str
 
-    origin_cors: str
+    allowed_origin_cors: str
 
     cache_host: str
     cache_password: str
@@ -22,8 +22,8 @@ class DefaultSettings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding='utf-8', extra='ignore')
 
     @property
-    def allowed_origins(self) -> typing.List[str]:
-        return [o.strip() for o in self.origin_cors.split(',') if o.strip()]
+    def allowed_cors(self) -> typing.List[str]:
+        return [o.strip() for o in self.allowed_origin_cors.split(',') if o.strip()]
 
 
 @lru_cache
